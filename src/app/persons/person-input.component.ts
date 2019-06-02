97919f62282ba3c16a43e3ec94c1a5e8fe895305
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-06-02 22:37:29 +0200

    button clicked

--- src/app/persons/person-input.component.ts
+++ src/app/persons/person-input.component.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-person-input',
-  templateUrl: './person-input.component.html'
+  templateUrl: './person-input.component.html',
+  styleUrls: ['./person-input.component.css']
 })
 
-export class PersonInputComponent { }
+export class PersonInputComponent {
+  onCreatePerson() {
+    console.log("Created");
+
+  }
+ }
