057f1da2fb73371ade6987677770e61cf9fecdb1
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-06-04 22:44:14 +0200

    push mechanism subscribe and unsubscribe

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,13 +1,15 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, OnInit, OnDestroy } from '@angular/core';
 import {PersonsService} from './persons.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-persons',
   templateUrl: './persons.component.html'
 })
 
-export class PersonsComponent implements OnInit {
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[];
+  private personListSubs: Subscription;
   //private personService: PersonsService;
 
   constructor(private prsService: PersonsService ) {
@@ -17,10 +19,17 @@ export class PersonsComponent implements OnInit {
 
 ngOnInit() {
   this.personList = this.prsService.persons;
+  this.personListSubs = this.prsService.personsChanged.subscribe(persons => {
+    this.personList = persons;
+  });
 }
 
 onRemovePerson(personName: string) {
   this.prsService.removePerson(personName);
+}
+
+ngOnDestroy() {
+  this.personListSubs.unsubscribe();
 }
 
 }
--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,21 +1,23 @@
 import {Injectable} from '@angular/core';
+import { Subject } from 'rxjs';
 
 
 @Injectable({providedIn: 'root'})
 
 export class PersonsService {
+  personsChanged = new Subject<string[]>();
   persons: string[] = ['Max', 'Dilshod', 'Nodira'];
 
   addPerson(name: string) {
     this.persons.push(name);
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);
   }
 
   removePerson(name: string) {
     this.persons = this.persons.filter(person => {
       return person !== name;
     });
-    console.log(this.persons);
+    this.personsChanged.next(this.persons);
 
   }
 }
