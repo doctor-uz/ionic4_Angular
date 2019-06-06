f6454c3e349d02d875db1eef8d5406ea1f4e67ef
Dilshod Rahmatov <dilshodrahmatov@gmail.com>
2019-06-06 16:47:13 +0200

    http request rendering names

--- src/app/persons/persons.service.ts
+++ src/app/persons/persons.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 
 @Injectable({providedIn: 'root'})
@@ -12,7 +13,9 @@ export class PersonsService {
   constructor(private http: HttpClient) {}
 
   fetchPersons() {
-    this.http.get<any>('https://swapi.co/api/people/').subscribe(resData => console.log(resData)
+    this.http.get<any>('https://swapi.co/api/people/').pipe(map(resData => {
+return resData.results.map(character => character.name);
+    })).subscribe(transformedData => this.personsChanged.next(transformedData)
     );
   }
 
